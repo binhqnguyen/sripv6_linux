dba10f37347d79701d7a7d921fff1b2a20aa5ee8
Binh Nguyen <binh@cs.utah.edu>
2017-06-14 12:44:47 -0600

    Replace local IPv6 with global IPv6

--- get_info.sh
+++ get_info.sh
@@ -9,11 +9,11 @@ if [ "$hn" == "node1" ]; then
 	echo "#Node $hn..."
 	n1_a_dev=$($INF | grep neta | awk '{print $3}')
 	n1_a_mac=$(ifconfig | grep $n1_a_dev | awk '{print $5}')
-	n1_a=$(ifconfig | grep -A3 $n1_a_dev | grep inet6 | awk '{print $3}' | awk -F'/' '{print $1}')
+	n1_a=$(ifconfig | grep -A3 $n1_a_dev | grep inet6 | awk '{print $3}' | awk -F'/' '{print $1}' | sed s/fe80::/2001::/ )
 
 	n1_c_dev=$($INF | grep netc | awk '{print $3}')
 	n1_c_mac=$(ifconfig | grep $n1_c_dev | awk '{print $5}')
-	n1_c=$(ifconfig | grep -A3 $n1_c_dev | grep inet6 | awk '{print $3}' | awk -F'/' '{print $1}')
+	n1_c=$(ifconfig | grep -A3 $n1_c_dev | grep inet6 | awk '{print $3}' | awk -F'/' '{print $1}' | sed s/fe80::/2001::/ )
 	echo "n1_a=\"$n1_a\""
 	echo "n1_a_mac=\"$n1_a_mac\""
 	echo "n1_a_dev=\"$n1_a_dev\""
@@ -29,11 +29,11 @@ if [ "$hn" == "node2" ]; then
 	echo "#Node $hn..."
 	n2_a_dev=$($INF | grep neta | awk '{print $3}')
 	n2_a_mac=$(ifconfig | grep $n2_a_dev | awk '{print $5}')
-	n2_a=$(ifconfig | grep -A3 $n2_a_dev | grep inet6 | awk '{print $3}' | awk -F'/' '{print $1}')
+	n2_a=$(ifconfig | grep -A3 $n2_a_dev | grep inet6 | awk '{print $3}' | awk -F'/' '{print $1}' | sed s/fe80::/2001::/ )
 
 	n2_b_dev=$($INF | grep netb | awk '{print $3}')
 	n2_b_mac=$(ifconfig | grep $n2_b_dev | awk '{print $5}')
-	n2_b=$(ifconfig | grep -A3 $n2_b_dev | grep inet6 | awk '{print $3}' | awk -F'/' '{print $1}')
+	n2_b=$(ifconfig | grep -A3 $n2_b_dev | grep inet6 | awk '{print $3}' | awk -F'/' '{print $1}' | sed s/fe80::/2001::/ )
 	echo "n2_a=\"$n2_a\""
 	echo "n2_a_mac=\"$n2_a_mac\""
 	echo "n2_a_dev=\"$n2_a_dev\""
@@ -50,11 +50,11 @@ if [ "$hn" == "node3" ]; then
 	echo "#Node $hn..."
 	n3_b_dev=$($INF | grep netb | awk '{print $3}')
 	n3_b_mac=$(ifconfig | grep $n3_b_dev | awk '{print $5}')
-	n3_b=$(ifconfig | grep -A3 $n3_b_dev | grep inet6 | awk '{print $3}' | awk -F'/' '{print $1}')
+	n3_b=$(ifconfig | grep -A3 $n3_b_dev | grep inet6 | awk '{print $3}' | awk -F'/' '{print $1}' | sed s/fe80::/2001::/ )
 
 	n3_d_dev=$($INF | grep netd | awk '{print $3}')
 	n3_d_mac=$(ifconfig | grep $n3_d_dev | awk '{print $5}')
-	n3_d=$(ifconfig | grep -A3 $n3_d_dev | grep inet6 | awk '{print $3}' | awk -F'/' '{print $1}')
+	n3_d=$(ifconfig | grep -A3 $n3_d_dev | grep inet6 | awk '{print $3}' | awk -F'/' '{print $1}' | sed s/fe80::/2001::/ )
 	echo "n3_b=\"$n3_b\""
 	echo "n3_b_mac=\"$n3_b_mac\""
 	echo "n3_b_dev=\"$n3_b_dev\""
@@ -71,11 +71,11 @@ if [ "$hn" == "node4" ]; then
 	n4=$($INF)
 	n4_c_dev=$($INF | grep netc | awk '{print $3}')
 	n4_c_mac=$(ifconfig | grep $n4_c_dev | awk '{print $5}')
-	n4_c=$(ifconfig | grep -A3 $n4_c_dev | grep inet6 | awk '{print $3}' | awk -F'/' '{print $1}')
+	n4_c=$(ifconfig | grep -A3 $n4_c_dev | grep inet6 | awk '{print $3}' | awk -F'/' '{print $1}' | sed s/fe80::/2001::/ )
 
 	n4_d_dev=$($INF | grep netd | awk '{print $3}')
 	n4_d_mac=$(ifconfig | grep $n4_d_dev | awk '{print $5}')
-	n4_d=$(ifconfig | grep -A3 $n4_d_dev | grep inet6 | awk '{print $3}' | awk -F'/' '{print $1}')
+	n4_d=$(ifconfig | grep -A3 $n4_d_dev | grep inet6 | awk '{print $3}' | awk -F'/' '{print $1}' | sed s/fe80::/2001::/ )
 	echo "n4_c=\"$n4_c\""
 	echo "n4_c_mac=\"$n4_c_mac\""
 	echo "n4_c_dev=\"$n4_c_dev\""
