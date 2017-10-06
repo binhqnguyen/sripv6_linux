a42239ca42dadc7db236de102419786dc87f194b
Binh Nguyen <binh@cs.utah.edu>
2017-10-06 10:21:34 -0600

    copy get_interface_map.pl to home folder

--- frr-ovs/get_all_info.sh
+++ frr-ovs/get_all_info.sh
@@ -10,6 +10,7 @@ GET_INTERFACES_HELPER="$HOME/get_interface_map.pl"
 hn=$(hostname)
 domain=$(echo $hn | awk -F'.' '{print $2"."$3"."$4"."$5}')
 echo "Get info of node $hn ..."
+cp get_interface_map.pl $HOME/
 ./get_info.sh >> net_info.sh
 
 
